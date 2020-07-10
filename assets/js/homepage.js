4a0457b7ecf044c8934d5c8bbc9147548fc207a6
Jacob Daughtry <akutozo@gmail.com>
2020-07-09 21:25:54 -0500

    6.2.7

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,10 +8,20 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        displayRepos(data, user);
-      });
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+        response.json().then(function(data) {
+            displayRepos(data, user);
+        });
+        } else {
+        alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
     });
   };
  
@@ -37,6 +47,11 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 // Display Repos
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     //Display Search term on the page 6.2.5
