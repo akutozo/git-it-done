937bdaf7e5d409d7ec475c7ce0af99804167cb16
Jacob Daughtry <akutozo@gmail.com>
2020-07-11 08:22:03 -0500

    playing fetch with q

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,29 +1,44 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/"+ repo + "/issues?direction=asc";
     console.log(repo);
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
+        response.json().then(function(data) {
             displayIssues(data);
-
+    
             // check if api has paginated issues
-        if (response.headers.get("Link")) {
-             displayWarning(repo);
+            if (response.headers.get("Link")) {
+            displayWarning(repo);
+            }
+        });
+        } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
-          });
-        }
-        else {
-          alert("There was a problem with your request!");
-        }
-      });
+    });
 };
-  
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -73,3 +88,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
   };
+
+  getRepoName()
